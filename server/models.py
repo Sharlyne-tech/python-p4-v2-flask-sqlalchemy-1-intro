086b219a596dd82f4ce4c14ace13efb5d053579f
Sharlyne <your_email@example.com>
2025-06-25 17:58:03 +0300

    initial  commit

--- server/models.py
+++ server/models.py
@@ -1,18 +1,52 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
 
-# create the Flask SQLAlchemy extension
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
 db = SQLAlchemy(metadata=metadata)
 
-# define a model class by inheriting from db.Model.
 
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = 'customers'
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    species = db.Column(db.String)
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+
+    def __repr__(self):
+        return f'<Customer {self.id}, {self.name}>'
+
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = 'items'
+
+    serialize_rules = ('-reviews.item',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+    reviews = db.relationship('Review', back_populates='item')
+    customer = association_proxy('reviews', 'customer')
+
+    def __repr__(self):
+        return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__='reviews'
+
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
